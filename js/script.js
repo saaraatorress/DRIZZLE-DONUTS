5f7f02388a2906e2271c679024d495a3a7dc5b29
sarestmar <sarestmar@gmail.com>
2024-10-29 17:32:36 +0100

    carrusel.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,29 @@ function changeImage() {
         img.src = img1;
     }
 }
+
+
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,          // Muestra 3 imágenes a la vez
+    spaceBetween: 20,          // Espacio entre las imágenes
+    loop: true,                // Permite el bucle infinito
+    autoplay: {
+      delay: 3000,             // Cambia cada 3 segundos
+      disableOnInteraction: false, // Continúa automáticamente después de la interacción
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {             // Adaptación para diferentes tamaños de pantalla
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
